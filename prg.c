6737fc00c9c22ba8bd389a0d9f79f2277eb70d30
Manas <manassidnal123@gmail.com>
2025-12-05 03:46:45 +0530

    added helper function

--- prg.c
+++ prg.c
@@ -3,8 +3,13 @@
 int add(int x, int y){
     return x+y;
 }
+
+int sub(int x, int y){
+    return x-y;
+}
 int main() {
     int x = add(1, 2);
-    printf("%d", x);
+    int y = sub(2, 1);
+    printf("%d %d", x, y);
     return 0;
 }
