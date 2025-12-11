1493cefe915dd6eb66f8f67e947530d2d6666d58
Manas <manassidnal123@gmail.com>
2025-12-12 02:32:31 +0530

    testing revert.This reverts commit 6737fc00c9c22ba8bd389a0d9f79f2277eb70d30.

--- prg.c
+++ prg.c
@@ -3,13 +3,8 @@
 int add(int x, int y){
     return x+y;
 }
-
-int sub(int x, int y){
-    return x-y;
-}
 int main() {
     int x = add(1, 2);
-    int y = sub(2, 1);
-    printf("%d %d", x, y);
+    printf("%d", x);
     return 0;
 }
